9e511de037c36b6480f1c0e69c90b8f92ac1a102
JP Meyer <jp@jpmeyer.dev>
2022-02-11 15:54:53 +0200

    Added serverless handler

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const greetRoutes = require('./routes/greet');
+const serverless = require('serverless-http');
 
 const app = express();
 const port = 3000;
 
 app.use('/greet', greetRoutes);
 
-app.listen(port, () => {
-    console.log(`The app is listening on port ${port}, request a greeting!`);
-});
+// app.listen(port, () => {
+//     console.log(`The app is listening on port ${port}, request a greeting!`);
+// });
+module.exports.handler = serverless(app);
